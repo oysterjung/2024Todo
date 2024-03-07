ad6c215c4b35e9e1bef9a9b66ce2b4e5c55ef40a
oysterjung <resd877@naver.com>
2024-03-08 00:31:15 +0900

    Feat : input기능 추가(미완성)

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 function Input() {
+  const [todo, setTodo] = useState([
+    { id: 1, text: "learn web", checked: false },
+    { id: 2, text: "get a job", checked: false },
+  ]);
+
   return (
     <div className="flex">
+      {todo.map((article) => {
+        return (
+          <div key={article.id}>
+            <h1>{article.text}</h1>
+          </div>
+        );
+      })}
       <div>TODO LIST</div>
       <div>
         <input type="text" />
@@ -12,6 +25,13 @@ function Input() {
           <option value="Ing">Ing</option>
         </select>
         <button>달</button>
+        <button
+          onClick={() =>
+            setTodo([...todo, { id: 1, text: "예시제목", checked: false }])
+          }
+        >
+          데이터 추가하기
+        </button>
       </div>
     </div>
   );
