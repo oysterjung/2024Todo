1e571012606c9c479545575993fea57c2b258370
oysterjung <resd877@naver.com>
2024-03-15 00:14:38 +0900

    feat : input기능 넣는중.
    windows로 옮기는중

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Input from "./components/Input/input";
+import React from "react";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import { useState } from "react";
 
 function Input() {
-  const [todo, setTodo] = useState([
-    { id: 1, text: "learn web", checked: false },
-    { id: 2, text: "get a job", checked: false },
-  ]);
+  const [todo, setTodo] = useState([]);
 
   return (
     <div className="flex">
       {todo.map((article) => {
         return (
-          <div key={article.id}>
-            <h1>{article.text}</h1>
+          <div key={article}>
+            <h1>{article}</h1>
           </div>
         );
       })}
@@ -25,13 +22,7 @@ function Input() {
           <option value="Ing">Ing</option>
         </select>
         <button>달</button>
-        <button
-          onClick={() =>
-            setTodo([...todo, { id: 1, text: "예시제목", checked: false }])
-          }
-        >
-          데이터 추가하기
-        </button>
+        <button onClick={() => setTodo([])}>데이터 추가하기</button>
       </div>
     </div>
   );
